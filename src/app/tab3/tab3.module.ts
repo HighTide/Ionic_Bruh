91b38cce854b3cb8c0fb1575e3d268b228ae4f32
Floor <floorvermaat@hotmail.com>
2019-06-06 13:42:55 +0200

    Added bed modal and still working on the toast pop-up when the "Request new sheets" button is clicked

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { ModalPageBed } from './modal/tab3.modal.bed';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
+      RouterModule.forChild([{ path: '', component: Tab3Page }]),
+      RouterModule.forChild([{ path: '', component: ModalPageBed }])
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page, ModalPageBed]
 })
 export class Tab3PageModule {}
