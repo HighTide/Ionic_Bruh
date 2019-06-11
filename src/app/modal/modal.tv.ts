3aee2054ba813dec76d1ba728d86d927b67d4e88
Ragox <xfireblaster@ymail.com>
2019-06-11 17:52:25 +0200

    Modal tv creating data + toast properly implemented thanks to Charline
    I've added the variables that were to be used, so I plan on expanding on it.

--- src/app/modal/modal.tv.ts
+++ src/app/modal/modal.tv.ts
@@ -3,6 +3,8 @@ import { ModalController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 import { Console } from '@angular/core/src/console';
 
+
+
 @Component({
     template:
         `<ion-header>
@@ -17,27 +19,16 @@ import { Console } from '@angular/core/src/console';
             <ion-grid>
                 <ion-row>
                     <ion-col align="left">
-                        <ion-button class="center" shape="round"><ion-icon name="power"></ion-icon></ion-button>
+                        <ion-button class="center" shape="round" (click)=presentToast($true,$,$)><ion-icon name="power"></ion-icon></ion-button>
                     </ion-col>
                 </ion-row>
                 <ion-row>
                     <ion-col size="3">
                     </ion-col>
                     <ion-col class="center">
-                        <ion-button class="center">1</ion-button>
-                        <ion-button class="center">2</ion-button>
-                        <ion-button class="center">3</ion-button>
-                    </ion-col>
-                    <ion-col size="3">
-                    </ion-col>
-                </ion-row>
-                <ion-row>
-                    <ion-col size="3">
-                    </ion-col>
-                    <ion-col class="center">
-                        <ion-button class="center">4</ion-button>
-                        <ion-button class="center">5</ion-button>
-                        <ion-button class="center">6</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$1)>1</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$2)>2</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$3)>3</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -46,9 +37,20 @@ import { Console } from '@angular/core/src/console';
                     <ion-col size="3">
                     </ion-col>
                     <ion-col class="center">
-                        <ion-button class="center">7</ion-button>
-                        <ion-button class="center">8</ion-button>
-                        <ion-button class="center">9</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$4)>4</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$5)>5</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$6)>6</ion-button>
+                    </ion-col>
+                    <ion-col size="3">
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col size="3">
+                    </ion-col>
+                    <ion-col class="center">
+                        <ion-button class="center" (click)=presentToast($,$,$7)>7</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$8)>8</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$9)>9</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -57,7 +59,7 @@ import { Console } from '@angular/core/src/console';
                     <ion-col size="4.5">
                     </ion-col>
                     <ion-col size="3">
-                        <ion-button class="center">0</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$0)>0</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -65,15 +67,15 @@ import { Console } from '@angular/core/src/console';
                 <ion-row>
                     <ion-col>
                         Volume
-                        <ion-button class="center">Volume +</ion-button>
-                        <ion-button class="center">Volume -</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$)>Volume +</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$)>Volume -</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
                     <ion-col>
                         Channel
-                        <ion-button class="center">Next</ion-button>
-                        <ion-button class="center">Prev</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$)>Next</ion-button>
+                        <ion-button class="center" (click)=presentToast($,$,$)>Prev</ion-button>
                     </ion-col>
                 </ion-row>
                 <ion-row>
@@ -100,24 +102,47 @@ import { Console } from '@angular/core/src/console';
 
 
 export class ModalPageTvComponent {
+    activationg: boolean;
+    volumeg: number;
+    channelg: number;
 
-    constructor(private ctrl: ModalController) { }
+    constructor(private ctrl: ModalController, private ctrl_t: ToastController) {
+        this.volumeg = 0;
+        this.channelg = 0;
+        this.activationg = null;
+    }
 
     async close() {
         this.ctrl.dismiss();
     }
 
-    // Something ToastController Here
-}
+    async presentToast(activation: boolean, volume:number, channel:number) {
 
-export class ToastPageTv {
-    constructor(private ctrl: ToastController) { }
+        if (this.activationg == null) {
+            const toast = await this.ctrl_t.create({
+                message: 'The tv is off, turn it on',
+                duration: 2000,
+            });
+            this.activationg = activation;
+            toast.present();
+        }
+        if (this.activationg == false) {
+            const toast = await this.ctrl_t.create({
+                message: 'The tv is off',
+                duration: 2000
+            });
+            this.activationg = true;
+            toast.present();
+        }
+        if (this.activationg == true) {
+            const toast = await this.ctrl_t.create({
+                message: 'The tv is turned off',
+                duration: 2000
+            })
+            this.activationg = false;
+            toast.present();
+        }
 
-    async presentToast() {
-        const toast = await this.ctrl.create({
-            message: 'Channel has been set to ',
-            duration: 2000
-        });
-        toast.present();
     }
+    
 }
