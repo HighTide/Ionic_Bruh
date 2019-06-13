422808e6b552d9e2be40f45a1ad8fd5cacbf7268
Rick Bunte <rickbunte2000@gmail.com>
2019-06-13 20:08:49 +0200

    Tv Remote knoppen grotendeels functioneel [MERGE READY]
    Data wordt aangemaakt en het wordt ook tijdelijk opgeslagen. De data moet nog wel naar de server verstuurd worden, maar dat gebeurt zodra ik dat samen met Nathan kan doen.

--- src/app/modal/modal.tv.ts
+++ src/app/modal/modal.tv.ts
@@ -26,9 +26,9 @@ import { Console } from '@angular/core/src/console';
                     <ion-col size="3">
                     </ion-col>
                     <ion-col class="center">
-                        <ion-button class="center" (click)=presentToast($,$1)>1</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$2)>2</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$3)>3</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(1)>1</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(2)>2</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(3)>3</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -37,9 +37,9 @@ import { Console } from '@angular/core/src/console';
                     <ion-col size="3">
                     </ion-col>
                     <ion-col class="center">
-                        <ion-button class="center" (click)=presentToast($,$4)>4</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$5)>5</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$6)>6</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(4)>4</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(5)>5</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(6)>6</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -48,9 +48,9 @@ import { Console } from '@angular/core/src/console';
                     <ion-col size="3">
                     </ion-col>
                     <ion-col class="center">
-                        <ion-button class="center" (click)=presentToast($,$7)>7</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$8)>8</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$9)>9</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(7)>7</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(8)>8</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(9)>9</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -59,7 +59,7 @@ import { Console } from '@angular/core/src/console';
                     <ion-col size="4.5">
                     </ion-col>
                     <ion-col size="3">
-                        <ion-button class="center" (click)=presentToast($,$0)>0</ion-button>
+                        <ion-button class="center" (click)=channelNumInput(0)>0</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -67,8 +67,8 @@ import { Console } from '@angular/core/src/console';
                 <ion-row>
                     <ion-col>
                         Volume
-                        <ion-button class="center" (click)=presentToast($,$)>Volume +</ion-button>
-                        <ion-button class="center" (click)=presentToast($,$)>Volume -</ion-button>
+                        <ion-button class="center" (click)=volumeSwitch(1)>Volume +</ion-button>
+                        <ion-button class="center" (click)=volumeSwitch(2)>Volume -</ion-button>
                     </ion-col>
                     <ion-col size="3">
                     </ion-col>
@@ -107,13 +107,15 @@ export class ModalPageTvComponent {
     channelg: number;
 
     constructor(private ctrl: ModalController, private ctrl_t: ToastController) {
-
+        this.channelg = 0;
+        this.volumeg = 10;
     }
 
     async close() {
         this.ctrl.dismiss();
     }
 
+    //Tv activation
     async activationTV() {
         if (this.activationg == null || this.activationg == false) {
             const toast = await this.ctrl_t.create({
@@ -133,15 +135,7 @@ export class ModalPageTvComponent {
         }
     }
 
-    async test(num: number) {
-        var a = num.toString();
-        const toast = await this.ctrl_t.create({
-            message: a,
-            duration: 2000
-        });
-        toast.present();
-    }
-
+    //Channel Switching with the Next and Prev buttons
     async channelSwitch(channel: number) {
         if (this.activationg == null || this.activationg == false) {
             const toast = await this.ctrl_t.create({
@@ -151,16 +145,16 @@ export class ModalPageTvComponent {
             toast.present();
         }
         else {
-            if (channel == 1) {
-                this.channelg++;
+            if (this.channelg == 100 && channel == 1) {
+                this.channelg = 0;
                 const toast = await this.ctrl_t.create({
                     message: 'Channel set to ' + this.channelg.toString(),
                     duration: 2000
                 });
                 toast.present();
             }
-            if (this.channelg > 9 && channel == 1) {
-                this.channelg = 0;
+            else if (channel == 1) {
+                this.channelg++;
                 const toast = await this.ctrl_t.create({
                     message: 'Channel set to ' + this.channelg.toString(),
                     duration: 2000
@@ -175,7 +169,7 @@ export class ModalPageTvComponent {
                 });
                 toast.present();
             }
-            if (channel == 2 && this.channelg >= 1) {
+            else if (channel == 2 && this.channelg >= 1) {
                 this.channelg--;
                 const toast = await this.ctrl_t.create({
                     message: 'Channel set to ' + this.channelg.toString(),
@@ -186,7 +180,50 @@ export class ModalPageTvComponent {
         }  
     }
 
-    async presentToast(volume:number, channel:number) {
+    //Volume changing with the Volume + and Volume - buttons
+    async volumeSwitch(volume: number) {
+        if (this.activationg == null || this.activationg == false) {
+            const toast = await this.ctrl_t.create({
+                message: 'The tv is off, turn it on',
+                duration: 2000,
+            });
+            toast.present();
+        }
+        else {
+            if (this.volumeg == 100 && volume == 1) {
+                const toast = await this.ctrl_t.create({
+                    message: 'Maximum volume reached',
+                    duration: 2000
+                });
+                toast.present();
+            }
+            else if (volume == 1) {
+                this.volumeg++;
+                const toast = await this.ctrl_t.create({
+                    message: 'Volume set to ' + this.volumeg.toString(),
+                    duration: 2000
+                });
+                toast.present();
+            }
+            if ((this.volumeg == 0 || this.volumeg == null) && volume == 2) {
+                const toast = await this.ctrl_t.create({
+                    message: 'Minimum volume reached',
+                    duration: 2000
+                });
+                toast.present();
+            }
+            else if (volume == 2 && this.volumeg >= 1) {
+                this.volumeg--;
+                const toast = await this.ctrl_t.create({
+                    message: 'Volume set to ' + this.volumeg.toString(),
+                    duration: 2000
+                });
+                toast.present();
+            }
+        }
+    }
+
+    async channelNumInput(channel:number) {
 
         if (this.activationg == null || this.activationg == false) {
             const toast = await this.ctrl_t.create({
@@ -195,7 +232,14 @@ export class ModalPageTvComponent {
             });
             toast.present();
         }
-        
+        else {
+            this.channelg = channel;
+            const toast = await this.ctrl_t.create({
+                message: 'Channel set to ' + this.channelg.toString(),
+                duration: 2000
+            });
+            toast.present();
+        }
 
     }
     
