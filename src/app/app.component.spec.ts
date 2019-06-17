187e9e8b531f34ba2046a14ca6f279caa9a96f1b
Floor Vermaat (0922205) <0922205@hr.nl>
2019-06-17 13:24:51 +0200

    Karma complained about providers

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -48,29 +48,4 @@ describe('AppComponent', () => {
         expect(statusBarSpy.styleDefault).toHaveBeenCalled();
         expect(splashScreenSpy.hide).toHaveBeenCalled();
     });
-
-    //Testcase #1 "Check if the application is able to use the camera
-    it('should be able to use the camera', () => {
-        expect(Camera).toHaveBeenCalled();
-    });
-
-    //Testcase #2 "Check if the application retrieves a frame"
-
-    //Testcase #3 "Check if one object is detected by the application within the specified frame"
-
-    //Testcase #4 "Check if there is no object detected by the application within the specified frame"
-
-    //Testcase #5 "Check if the image has been sent to the image processing software"
-
-    //Testcase #6 "Check if the frame filters the tags, which contains the recognized objects of the uploaded frames"
-
-    //Testcase #7 "Check if the application is able to detect a lamp that is on"
-
-    //Testcase #8 "Check if the application is able to detect a lamp that is off"
-
-    //Testcase #9 "Check if the application gives a pop-up for a lamp after the application detects the lamp"
-
-
-    // TODO: add more tests!
-
 });
